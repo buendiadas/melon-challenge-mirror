a0ca9956d75ab1d74262b2b2eac9e72137c74b76
Carlos Buendía <buendiacarlos@gmail.com>
2020-07-31 03:02:35 +0200

    ci: Include mainnet and rinkeby forks

---
+++ config.js
@@ -0,0 +1,17 @@
+// ttl, active time and claimtime were increased in order to pass integration tests.
+
+module.exports = {
+  mainnnet: {
+    DAI: '0x6b175474e89094c44da98b954eedeac495271d0f',
+    DAI_COMPOUND: '0x6b175474e89094c44da98b954eedeac495271d0f',
+    CDAI: '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643',
+    COMP: '0xc00e94cb662c3520282e6f5717214004a7f26888',
+    COMPTROLLER: '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B'
+  },
+  rinkeby: {
+    DAI: '0x6b175474e89094c44da98b954eedeac495271d0f',
+    DAI_COMPOUND: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
+    CDAI: '0x6d7f0754ffeb405d23c51ce938289d4835be3b14',
+    COMP: '0xc00e94Cb662C3520282E6f5717214004A7f26888'
+  }
+}
